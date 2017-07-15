686069800e6728602aa734243c7ef11a3cb96b90
Deepjyoti <deepjyoti@hike.in>
2017-07-16 03:17:15 +0530

    async and sync action creators with redux-thunk, sync auth to firebase with redux-thunk

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,3 +1,5 @@
+import firebase from 'firebase';
+
 import {
   EMAIL_CHANGED,
   PASSWORD_CHANGED
@@ -15,4 +17,16 @@ export const passwordChangedAction = (text) => {
     type: PASSWORD_CHANGED,
     payload: text
   }
+}
+
+export const loginUser = ({ email, password }) => {
+  return (dispatch) => {
+    firebase.auth().signInWithEmailAndPassword(email, password)
+      .then(user => {
+        dispatch({
+          type: 'LOGIN_USER_SUCCESS',
+          payload: user
+        })
+      });
+  };
 }
--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Card, CardSection, Input, Button } from './common';
-import { emailChangedAction, passwordChangedAction } from '../actions';
+import { emailChangedAction, passwordChangedAction, loginUser } from '../actions';
 
 class LoginForm extends Component {
 
@@ -11,6 +11,11 @@ class LoginForm extends Component {
 
   onPasswordChange(text) {
     this.props.passwordChangedAction(text);
+  }
+
+  onButtonPress() {
+    const { email, password } = this.props;
+    this.props.loginUser({ email, password });
   }
 
   render() {
@@ -36,7 +41,7 @@ class LoginForm extends Component {
         </CardSection>
 
         <CardSection>
-          <Button>
+          <Button onPress={this.onButtonPress.bind(this)}>
             Login
           </Button>
         </CardSection>
@@ -53,4 +58,8 @@ const mapStateToProps = state => {
   }
 }
 
-export default connect(mapStateToProps, { emailChangedAction, passwordChangedAction})(LoginForm);
+export default connect(mapStateToProps, {
+  emailChangedAction,
+  passwordChangedAction,
+  loginUser
+})(LoginForm);
