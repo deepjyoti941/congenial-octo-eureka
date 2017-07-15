42a76fd18186e760eb8933cd987c6c9b700f7ce1
Deepjyoti <deepjyoti@hike.in>
2017-07-16 03:21:52 +0530

    make user login and set state from reducer

--- src/reducers/AuthReducer.js
+++ src/reducers/AuthReducer.js
@@ -1,11 +1,13 @@
 import {
   EMAIL_CHANGED,
-  PASSWORD_CHANGED
+  PASSWORD_CHANGED,
+  LOGIN_USER_SUCCESS
 } from '../actions/types';
 
 const INITIAL_STATE = {
   email: '',
-  password: ''
+  password: '',
+  user: null
 };
 
 export default (state = INITIAL_STATE, action) => {
@@ -16,6 +18,9 @@ export default (state = INITIAL_STATE, action) => {
 
     case PASSWORD_CHANGED:
       return { ...state, password: action.payload };
+
+    case LOGIN_USER_SUCCESS:
+      return { ...state, user: action.payload };
 
     default:
       return state;
